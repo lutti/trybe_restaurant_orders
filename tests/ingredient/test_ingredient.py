8fbb0d3decdff72e06609325344c0de803aa713e
Lutti <lutti.c.amaral@gmail.com>
2023-08-26 13:45:38 -0300

    Ingredient Test

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,17 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1 - Lutti
 def test_ingredient():
-    pass
+    ing_1 = Ingredient("picanha")
+    ing_2 = Ingredient("setinho")
+    ing_3 = Ingredient("picanha")
+
+    assert ing_1.name == "picanha"
+    assert ing_1.restrictions == set()
+
+    assert hash(ing_1) != hash(ing_2)
+    assert hash(ing_1) == hash(ing_3)
+
+    assert ing_1 == ing_3
+    assert ing_1 != ing_2
+
+    assert repr(ing_1) == "Ingredient('picanha')"
